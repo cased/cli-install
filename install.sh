a28061e7cdc2898ee5ed3457ac3f6566f1c4c0b3
tnm <ted@ted.io>
2021-01-25 20:54:42 -0800

    Use /tmp and update help

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -u
+set -e
+
+trap 'message' ERR
+
+message() {
+  echo "[cased-install] An error has occurred. If you received a 'Permission Denied' error, you may need to run this script as sudo."
+  echo "[cased-install] Please also visit https://docs.cased.com/docs/troubleshooting for more help."
+}
 
 rm -rf tmp-guard
-git clone https://github.com/cased/guard tmp-guard
-cd tmp-guard
+git clone https://github.com/cased/guard /tmp/tmp-guard
+cd /tmp/tmp-guard
 
 ./install
 cd ..
-rm -rf tmp-guard
+rm -rf /tmp/tmp-guard
